03005f7995edf74d5d2451ae673655b0ceb595fc
Phrygien <84671612+phrygien@users.noreply.github.com>
2023-05-11 12:15:09 +0200

    mise en place model constat poulet

---
+++ app/Models/ConstatPoulet.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ConstatPoulet extends Model
+{
+    use HasFactory;
+}
